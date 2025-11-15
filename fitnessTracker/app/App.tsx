34fae413505e8d59f573f34d4777791ced625f6c
leonard-roepcke <leonard.roepcke.2@gmail.com>
2025-11-15 02:56:33 +0100

    new routing

--- fitnessTracker/app/App.tsx
+++ fitnessTracker/app/App.tsx
@@ -1,7 +1,15 @@
-// app/App.tsx
-import React from 'react';
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OverviewScreen from './screens/OverviewScreen';
+import WorkoutScreen from './screens/WorkoutScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <OverviewScreen />;
+  return (
+    <Stack.Navigator initialRouteName="Overview">
+      <Stack.Screen name="Overview" component={OverviewScreen} />
+      <Stack.Screen name="Workout" component={WorkoutScreen} />
+    </Stack.Navigator>
+  );
 }
---
+++ fitnessTracker/app/screens/WorkoutScreen.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { View,ScrollView, Text, StyleSheet } from 'react-native';
+
+
+export default function WorkoutScreen() {
+    return (
+        <View>
+            <ScrollView>
+                <Text>Workout X</Text>
+            </ScrollView>
+        </View>
+    );
+}
