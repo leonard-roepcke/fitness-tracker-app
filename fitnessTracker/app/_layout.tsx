f76585cd6aab35a99e003d0951f6512848b59c7a
leonard-roepcke <leonard.roepcke.2@gmail.com>
2025-11-24 05:48:38 +0100

    animation overlay not seable

--- fitnessTracker/app/_layout.tsx
+++ fitnessTracker/app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from 'expo-router';
 import { WorkoutProvider } from "../context/WorkoutContext";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
   return (
     <WorkoutProvider>
-      <Stack screenOptions={{headerShown: false}}/>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack screenOptions={{headerShown: false}}/>
+      </GestureHandlerRootView>
     </WorkoutProvider>
   );
 }
