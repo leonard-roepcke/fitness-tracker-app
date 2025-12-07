92a6c7343c3cd34720c37c9e45c1a9e59a91e151
leonard-roepcke <leonard.roepcke.2@gmail.com>
2025-12-07 15:21:09 +0100

    consts update

---
+++ fitnessTracker/app/types/calorys.ts
@@ -0,0 +1,6 @@
+export interface CaloryEntry {
+    id: number;
+    date: number;
+    calorys: number;
+}
+export type Calorys = CaloryEntry[];
--- fitnessTracker/context/TrackerContext.tsx
+++ fitnessTracker/context/TrackerContext.tsx
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { createContext, ReactNode, useContext, useEffect, useState } from "react";
-import { WorkoutLogsByDate } from "../app/types/tracker";
+import { WorkoutLogsByDate } from "../app/types/workoutLogs";
 import { Workout } from "./WorkoutContext"; // Typ aus deinem WorkoutContext
 
 const STORAGE_KEY = "@workout_logs_by_date";
