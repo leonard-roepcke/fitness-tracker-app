16dc7d0a2fda0f68563f5184f8c13c949fcce37d
leonard-roepcke <leonard.roepcke.2@gmail.com>
2025-12-12 17:40:35 +0100

    added Stats to Health screen

--- fitnessTracker/app/screens/HealthScreen.tsx
+++ fitnessTracker/app/screens/HealthScreen.tsx
@@ -1,9 +1,50 @@
-import { View , Text } from "react-native";
+import { ThemeContext } from '@/context/ThemeContext';
+import { View , Text , StyleSheet} from "react-native";
 import AppContainer from "../components/ui/AppContainer";
+import { useContext } from "react";
+import CStats from '../components/CStats';
+import WStats from '../components/WStats';
+import { useAppContext } from '../hooks/useAppContext';
 
 export default function HealthScreen(){
+  const { isWTrackerEnabled, isCTrackerEnabled } = useContext(ThemeContext);
+      const { colors, layouts} = useAppContext();
+     const styles = StyleSheet.create({
+       title: {
+            fontSize: 10,
+            fontWeight: '600',
+            marginBottom: layouts.marginVertical,
+            color: colors.text,
+            justifyContent: 'center',
+            alignItems: 'center',
+
+        },
+        subtitle: {
+            fontSize: 16,
+            color: colors.textSecondary,
+        },
+        scrollView: {
+            width: '100%',
+        },
+        header: {
+            fontSize: 32,
+            fontWeight: "bold",
+            marginTop: 35,
+            color: colors.text,
+        },
+        spacing: {
+            height: layouts.marginVertical*12,
+        }
+    });
+
+
+  
   return(
       <AppContainer isBar={true} heading="Health">
+                 <Text style={styles.subtitle}></Text>
+                {isWTrackerEnabled && <WStats />}
+                {isCTrackerEnabled && <CStats />}
+                <Text style={styles.subtitle}></Text>
 
     </AppContainer>
   )  
