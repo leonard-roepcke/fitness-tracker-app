b0a04211725a5a7c3e2db0d5a4fd26467547941f
leonard-roepcke <leonard.roepcke.2@gmail.com>
2025-12-13 13:38:24 +0100

    Language hook

--- fitnessTracker/app/hooks/useLanguage.ts
+++ fitnessTracker/app/hooks/useLanguage.ts
@@ -1,8 +1,13 @@
 import { LanguageContext } from "@/context/LanguageContext";
-import { useContext } from "react"
-
-
+import { useContext } from "react";
 
 export const useLanguage = () => {
-  const { language, setLanguage } = useContext(LanguageContext);
-}
+  const ctx = useContext(LanguageContext);
+
+  if (!ctx) {
+    throw new Error("useLanguage must be used inside LanguageProvider");
+  }
+
+  return ctx;
+};
+
